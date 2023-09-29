db62b46085f94fd08f60469e6e04e2b35195f9bd
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2023-09-28 23:52:58 -0300

    refactor: servo speed on open close function

--- esp8266-webServer.ino
+++ esp8266-webServer.ino
@@ -55,12 +55,12 @@ void controlServo()
     for (pos = 0; pos <= 180; pos += 1)
     {
       myservo.write(pos);
-      delay(15);
+      delay(30);
     }
     for (pos = 180; pos >= 0; pos -= 1)
     {
       myservo.write(pos);
-      delay(15);
+      delay(30);
     }
   }
   else if (t_state == "2")
